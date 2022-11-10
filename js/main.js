aadbc7a4f730188e7f67245f4d1bf75829cc48af
haijun.ma <ucmhj@163.com>
2022-11-10 13:42:12 +0800

    1

--- js/main.js
+++ js/main.js
@@ -13,23 +13,23 @@ myImage.onclick = function () {
         myImage.setAttribute('src','img/ani01.jpg');
     }
 };
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector('h1');
-function setUserName() {
-    let myName = prompt('请输入你的名字');
-    if (!myName){
-        setUserName()
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.textContent = 'mozilla 酷毙了,' + myName;
-    }
-}
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla 酷毙了,' + storedName;
-}
-myButton.onclick = function () {
-    setUserName();
-}
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector('h1');
+// function setUserName() {
+//     let myName = prompt('请输入你的名字');
+//     if (!myName){
+//         setUserName()
+//     } else {
+//         localStorage.setItem('name', myName);
+//         myHeading.textContent = 'mozilla 酷毙了,' + myName;
+//     }
+// }
+// if (!localStorage.getItem('name')) {
+//     setUserName();
+// } else {
+//     let storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Mozilla 酷毙了,' + storedName;
+// }
+// myButton.onclick = function () {
+//     setUserName();
+// }
